549b4bd01c5ff98acc1d8966dfb808bef95beba9
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-10-15 23:45:22 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package TabelaHash;
+
+public class Main {
+    public static void main(String[] args) {
+        Hash tabela = new Hash();
+
+        int[] valores = {12, 25, 37, 41, 53, 64, 78, 89};
+
+        for (int valor : valores) {
+            tabela.add(valor);
+        }
+
+        // Exibe a tabela após inserção
+        System.out.println("Tabela após inserção:");
+        tabela.printHashTable();
+
+        // Busca por alguns valores
+        System.out.println("\nBuscando valores:");
+        System.out.println("Encontrou 25? " + tabela.find(25));
+        System.out.println("Encontrou 50? " + tabela.find(50));
+
+        // Remoção de um valor
+        System.out.println("\nRemovendo 25:");
+        tabela.remove(25);
+
+        // Exibe a tabela após remoção
+        System.out.println("Tabela após remoção:");
+        tabela.printHashTable();
+    }
+}
